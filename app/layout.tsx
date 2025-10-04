11c461b5614f7cded53251f74f92fbc54d644085
Saiful Abidin <contact@sabidzpro.is-a.dev>
2025-10-05 02:42:08 +0700

    feat: integrate Google Tag Manager for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 import localFont from "next/font/local";
 import "./globals.css";
 import "./maps.css";
@@ -46,11 +47,35 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PXFQVC47"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Providers>
           {children}
         </Providers>
         <CookieConsent />
         <Analytics />
+
+        {/* Google Tag Manager Script */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PXFQVC47');`,
+          }}
+        />
       </body>
     </html>
   );
